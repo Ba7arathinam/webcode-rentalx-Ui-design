354d1f6004c76d0cb31ad810f818aec2db37412d
Ba7arathinam <bala01225@gmail.com>
2023-01-22 23:15:24 +0530

    do some correction on my files

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,13 +5,13 @@ export default function Navbar() {
   return (
     <nav className="navbar bg-dark navbar-dark">
       <div className="container-fluid">
-        <a className="d-flex navbar-brand">
+        <a className="d-flex navbar-brand" href="">
           <img
             style={{
               margin: "0 10px 0 0",
             }}
             src="https://cdn2.iconfinder.com/data/icons/realestate-vol1-yellow-shadow-bukeicon/32/rent_house_property_bukeicon-512.png"
-            alt="pizza-logo"
+            alt="app-logo"
             width="30"
             height="30"
             className="d-inline-block align-text-top"
